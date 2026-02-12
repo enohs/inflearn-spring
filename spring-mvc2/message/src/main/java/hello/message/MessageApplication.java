cb4bdeba16cefce20a4f6994f8e08a27858ba79c
enohs <kmsn01@naver.com>
2026-02-12 13:47:14 +0900

    add: 메시지, 국제화 학습 완료

--- spring-mvc2/message/src/main/java/hello/message/MessageApplication.java
+++ spring-mvc2/message/src/main/java/hello/message/MessageApplication.java
@@ -2,6 +2,9 @@ package hello.message;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class MessageApplication {
@@ -10,4 +13,13 @@ public class MessageApplication {
 		SpringApplication.run(MessageApplication.class, args);
 	}
 
+	// 스프링이 자동으로 등록해줌
+	/*@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new
+				ResourceBundleMessageSource();
+		messageSource.setBasenames("messages", "errors");
+		messageSource.setDefaultEncoding("utf-8");
+		return messageSource;
+	}*/
 }
