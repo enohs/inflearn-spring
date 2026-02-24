c85a7b3671ebb2f71c8c5fddf74e8ebc82e8f821
enohs <kmsn01@naver.com>
2026-02-24 17:48:36 +0900

    add: Jdbc 템플릿 및 테스트 방법 학습

--- spring-db2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ spring-db2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,26 +1,44 @@
 package hello.itemservice;
 
-import hello.itemservice.config.*;
+import hello.itemservice.config.JdbcTemplateV3Config;
 import hello.itemservice.repository.ItemRepository;
+import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-
-@Import(MemoryConfig.class)
-@SpringBootApplication(scanBasePackages = "hello.itemservice.web")
+@Slf4j
+//@Import(MemoryConfig.class)
+//@Import(JdbcTemplateV1Config.class) // 수동 빈등록 클래스로 설정
+//@Import(JdbcTemplateV2Config.class)
+@Import(JdbcTemplateV3Config.class)
+@SpringBootApplication(scanBasePackages = "hello.itemservice.web") // 서비스와 레포지토리는 수동등록을 위해 컨트롤러 패키지로 스캔 제한
 public class ItemServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ItemServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ItemServiceApplication.class, args);
+  }
 
-	@Bean
-	@Profile("local")
-	public TestDataInit testDataInit(ItemRepository itemRepository) {
-		return new TestDataInit(itemRepository);
-	}
+  @Bean // EventListener 사용을 위한 빈 등록
+  @Profile("local") // 특정 프로필이 활성화되어 있을 때만 빈으로 등록하는 기능. 해당 설정은 properties에서 함
+  public TestDataInit testDataInit(ItemRepository itemRepository) {
+    return new TestDataInit(itemRepository);
+  }
 
+  // 임베디드 모드 DB 사용을 위한 수동 빈 등록
+  /*@Bean
+  @Profile("test")
+  public DataSource dataSource() {
+    log.info("메모리 데이터베이스 초기화");
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName("org.h2.Driver");
+    dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+    dataSource.setUsername("sa");
+    dataSource.setPassword("");
+    return dataSource;
+  }*/
 }
--- spring-db2/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ spring-db2/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -1,92 +1,111 @@
 package hello.itemservice.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
+// 원래 서비스에서 쓰던 대로라면 로직이 성공하면 커밋해야하는데 지금 롤백을 하고 있다!
+// 테스트에서 사용하면 예외적으로 트랜잭션을 자동으로 롤백해주기 때문 (트랜잭션 생성과 롤백을 자동으로 처리해줌)
+// 사실상 DB 정보를 따로 설정하지 않으면 스프링이 임베디드 DB를 사용하기 때문에 @Transactional 하나만 붙이면 DB테스트가 가능해진다 (대신 resources에 schema.sql 파일 안에 초기 생성을 위한 테이블 DDL이 있어야 함)
+@Transactional
 @SpringBootTest
 class ItemRepositoryTest {
 
-    @Autowired
-    ItemRepository itemRepository;
+  @Autowired
+  ItemRepository itemRepository;
 
-    @AfterEach
-    void afterEach() {
-        //MemoryItemRepository 의 경우 제한적으로 사용
-        if (itemRepository instanceof MemoryItemRepository) {
-            ((MemoryItemRepository) itemRepository).clearStore();
-        }
+/*  @Autowired
+  PlatformTransactionManager transactionManager;
+  TransactionStatus status;
+
+  @BeforeEach
+  void beforeEach() {
+    // 트랜잭션 시작
+    status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+  }
+ */
+
+  @AfterEach
+  void afterEach() {
+    //MemoryItemRepository 의 경우 제한적으로 사용
+    if (itemRepository instanceof MemoryItemRepository) {
+      ((MemoryItemRepository) itemRepository).clearStore();
     }
 
-    @Test
-    void save() {
-        //given
-        Item item = new Item("itemA", 10000, 10);
+    // 트랜잭션 롤백
+    //transactionManager.rollback(status);
+  }
 
-        //when
-        Item savedItem = itemRepository.save(item);
+  //@Commit // 중간에 롤백이 아니라 커밋 하고 싶으면 @Transactional + @Commit을 사용하면 된다
+  @Test
+  void save() {
+    //given
+    Item item = new Item("itemA", 10000, 10);
 
-        //then
-        Item findItem = itemRepository.findById(item.getId()).get();
-        assertThat(findItem).isEqualTo(savedItem);
-    }
+    //when
+    Item savedItem = itemRepository.save(item);
 
-    @Test
-    void updateItem() {
-        //given
-        Item item = new Item("item1", 10000, 10);
-        Item savedItem = itemRepository.save(item);
-        Long itemId = savedItem.getId();
+    //then
+    Item findItem = itemRepository.findById(item.getId()).get();
+    assertThat(findItem).isEqualTo(savedItem);
+  }
 
-        //when
-        ItemUpdateDto updateParam = new ItemUpdateDto("item2", 20000, 30);
-        itemRepository.update(itemId, updateParam);
+  @Test
+  void updateItem() {
+    //given
+    Item item = new Item("item1", 10000, 10);
+    Item savedItem = itemRepository.save(item);
+    Long itemId = savedItem.getId();
 
-        //then
-        Item findItem = itemRepository.findById(itemId).get();
-        assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
-        assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
-        assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
-    }
+    //when
+    ItemUpdateDto updateParam = new ItemUpdateDto("item2", 20000, 30);
+    itemRepository.update(itemId, updateParam);
 
-    @Test
-    void findItems() {
-        //given
-        Item item1 = new Item("itemA-1", 10000, 10);
-        Item item2 = new Item("itemA-2", 20000, 20);
-        Item item3 = new Item("itemB-1", 30000, 30);
+    //then
+    Item findItem = itemRepository.findById(itemId).get();
+    assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
+    assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+    assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
+  }
 
-        itemRepository.save(item1);
-        itemRepository.save(item2);
-        itemRepository.save(item3);
+  @Test
+  void findItems() {
+    //given
+    Item item1 = new Item("itemA-1", 10000, 10);
+    Item item2 = new Item("itemA-2", 20000, 20);
+    Item item3 = new Item("itemB-1", 30000, 30);
 
-        //둘 다 없음 검증
-        test(null, null, item1, item2, item3);
-        test("", null, item1, item2, item3);
+    itemRepository.save(item1);
+    itemRepository.save(item2);
+    itemRepository.save(item3);
 
-        //itemName 검증
-        test("itemA", null, item1, item2);
-        test("temA", null, item1, item2);
-        test("itemB", null, item3);
+    //둘 다 없음 검증
+    test(null, null, item1, item2, item3);
+    test("", null, item1, item2, item3);
 
-        //maxPrice 검증
-        test(null, 10000, item1);
+    //itemName 검증
+    test("itemA", null, item1, item2);
+    test("itemA", null, item1, item2);
+    test("itemB", null, item3);
 
-        //둘 다 있음 검증
-        test("itemA", 10000, item1);
-    }
+    //maxPrice 검증
+    test(null, 10000, item1);
 
-    void test(String itemName, Integer maxPrice, Item... items) {
-        List<Item> result = itemRepository.findAll(new ItemSearchCond(itemName, maxPrice));
-        assertThat(result).containsExactly(items);
-    }
+    //둘 다 있음 검증
+    test("itemA", 10000, item1);
+  }
+
+  void test(String itemName, Integer maxPrice, Item... items) {
+    List<Item> result = itemRepository.findAll(new ItemSearchCond(itemName, maxPrice));
+    assertThat(result).containsExactly(items);
+  }
 }
