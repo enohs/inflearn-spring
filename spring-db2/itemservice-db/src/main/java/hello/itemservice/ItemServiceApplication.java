3ccf21e9e2a6b3a6fd04cbb687b531bd9cdf1a9f
enohs <kmsn01@naver.com>
2026-02-26 02:57:24 +0900

    add: query dsl 학습

--- spring-db2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ spring-db2/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,7 @@
 package hello.itemservice;
 
 import hello.itemservice.config.JpaConfig;
+import hello.itemservice.config.QuerydslConfig;
 import hello.itemservice.config.SpringDataJpaConfig;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -17,7 +18,8 @@ import org.springframework.context.annotation.Profile;
 //@Import(JdbcTemplateV3Config.class)
 //@Import(MyBatisConfig.class)
 //@Import(JpaConfig.class)
-@Import(SpringDataJpaConfig.class)
+//@Import(SpringDataJpaConfig.class)
+@Import(QuerydslConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web") // 서비스와 레포지토리는 수동등록을 위해 컨트롤러 패키지로 스캔 제한
 public class ItemServiceApplication {
 
