9a0d570b9cadb60f1d1b667009187dd12fe38e2b
enohs <kmsn01@naver.com>
2026-05-18 23:48:01 +0900

    feat: 스프링 데이터 JPA 및 querydsl 맛보기

--- spring-db2/itemservice-db/src/main/java/hello/itemservice/config/V2Config.java
+++ spring-db2/itemservice-db/src/main/java/hello/itemservice/config/V2Config.java
@@ -1,11 +1,11 @@
 package hello.itemservice.config;
 
 import hello.itemservice.repository.ItemRepository;
-import hello.itemservice.repository.jpa.JpaItemRepositoryV1;
-import hello.itemservice.repository.jpa.JpaItemRepositoryV2;
-import hello.itemservice.repository.jpa.SpringDataJpaItemRepository;
+import hello.itemservice.repository.jpa.JpaItemRepositoryV3;
+import hello.itemservice.repository.v2.ItemQueryRepositoryV2;
+import hello.itemservice.repository.v2.ItemRepositoryV2;
 import hello.itemservice.service.ItemService;
-import hello.itemservice.service.ItemServiceV1;
+import hello.itemservice.service.ItemServiceV2;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -13,18 +13,25 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class SpringDataJpaConfig {
+public class V2Config {
 
-  // JpaRepository를 상속 받았기 때문에 프록시 객체를 주입받음
-  private final SpringDataJpaItemRepository repository;
+  private final EntityManager em;
+  private final ItemRepositoryV2 itemRepositoryV2; // Spring Data Jpa에서 제공 (JPA Repository 상속 받으면 빈등록 자동이기 때문에 주입 받기만 하면 됨)
 
   @Bean
   public ItemService itemService() {
-    return new ItemServiceV1(itemRepository());
+    return new ItemServiceV2(itemRepositoryV2, itemQueryRepositoryV2());
   }
 
   @Bean
-  public ItemRepository itemRepository() {
-    return new JpaItemRepositoryV2(repository);
+  public ItemQueryRepositoryV2 itemQueryRepositoryV2() {
+    return new ItemQueryRepositoryV2(em);
   }
+
+  // 이거는 테스트 데이터 초기화 기능 때문에 필요한 설정
+  @Bean
+  public ItemRepository itemRepository() {
+    return new JpaItemRepositoryV3(em);
+  }
+
 }
--- spring-db2/itemservice-db/src/main/java/hello/itemservice/repository/v2/ItemRepositoryV2.java
+++ spring-db2/itemservice-db/src/main/java/hello/itemservice/repository/v2/ItemRepositoryV2.java
@@ -1,4 +1,8 @@
 package hello.itemservice.repository.v2;
 
-public class ItemRepositoryV2 {
+import hello.itemservice.domain.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepositoryV2 extends JpaRepository<Item, Long> {
+
 }
