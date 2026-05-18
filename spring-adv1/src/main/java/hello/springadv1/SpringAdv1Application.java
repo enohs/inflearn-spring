ca27a915c76ee6e79f2daf95c039dfc2f7dbd601
enohs <kmsn01@naver.com>
2026-05-18 23:40:22 +0900

    init: 스프링 핵심 원리 학습 세팅

---
+++ spring-adv1/src/main/java/hello/springadv1/SpringAdv1Application.java
@@ -0,0 +1,13 @@
+package hello.springadv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdv1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringAdv1Application.class, args);
+  }
+
+}
---
+++ spring-adv1/src/test/java/hello/springadv1/SpringAdv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springadv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdv1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
