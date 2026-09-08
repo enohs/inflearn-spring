64c846980e445f4709f88a34e4099fb352f514da
enohs <kmsn01@naver.com>
2026-09-08 14:52:09 +0900

    init: 스프링 라이브러리  관리 방식 학습 세팅 및 학습

---
+++ springboot/springboot-lib/src/main/java/hello/springbootlib/SpringbootLibApplication.java
@@ -0,0 +1,13 @@
+package hello.springbootlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLibApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootLibApplication.class, args);
+  }
+
+}
