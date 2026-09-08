bae64ab95210734b56723b33bd327807849bdd50
enohs <kmsn01@naver.com>
2026-09-08 13:22:48 +0900

    init: 스프링부트 파헤치기 세팅

---
+++ springboot/springboot-boot/src/main/java/hello/springbootboot/SpringbootBootApplication.java
@@ -0,0 +1,13 @@
+package hello.springbootboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootBootApplication.class, args);
+  }
+
+}
---
+++ springboot/springboot-boot/src/test/java/hello/springbootboot/SpringbootBootApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springbootboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBootApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
