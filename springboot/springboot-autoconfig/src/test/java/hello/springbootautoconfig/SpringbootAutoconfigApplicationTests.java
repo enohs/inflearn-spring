154dab5b4d79b13bd29796e679c7d3d6e1f67c84
enohs <kmsn01@naver.com>
2026-09-08 23:49:43 +0900

    init: auto configuration 학습 세팅

---
+++ springboot/springboot-autoconfig/src/test/java/hello/springbootautoconfig/SpringbootAutoconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springbootautoconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAutoconfigApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
