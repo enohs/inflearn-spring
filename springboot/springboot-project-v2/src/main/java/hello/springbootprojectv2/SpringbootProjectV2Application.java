d39fa9d89ee454a173ad2da9bc955b4c01abbc71
enohs <kmsn01@naver.com>
2026-09-11 10:32:52 +0900

    init: 자동 구성 실습을 위한 세팅

---
+++ springboot/springboot-project-v2/src/main/java/hello/springbootprojectv2/SpringbootProjectV2Application.java
@@ -0,0 +1,13 @@
+package hello.springbootprojectv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProjectV2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootProjectV2Application.class, args);
+  }
+
+}
---
+++ springboot/springboot-project-v2/src/test/java/hello/springbootprojectv2/SpringbootProjectV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springbootprojectv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProjectV2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
