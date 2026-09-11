cf8b82f91ba358b1df196998f1e0e343f7cd6565
enohs <kmsn01@naver.com>
2026-09-11 17:14:04 +0900

    init: 외부설정과 프로필 학습을 위한 세팅

---
+++ springboot/springboot-external/src/main/java/hello/springbootexternal/SpringbootExternalApplication.java
@@ -0,0 +1,13 @@
+package hello.springbootexternal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExternalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootExternalApplication.class, args);
+  }
+
+}
---
+++ springboot/springboot-external/src/test/java/hello/springbootexternal/SpringbootExternalApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springbootexternal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExternalApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
